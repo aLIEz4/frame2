281919fc3d7ecef0cd3ccc39d5d2485829823c12
PC03 <PC03@DESKTOP-93QC8HG>
2018-01-24 16:19:37 -0800

    yoo this is my first commint git bor

---
+++ src/firstpro/frame1.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstpro;
+
+/**
+ *
+ * @author PC03
+ */
+public class frame1 {
+    cara nk push project di netbeans ke github.com
+    
+}
